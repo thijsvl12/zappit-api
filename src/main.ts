6bcfa0b8fb151578fe9b656f6bcee23472dcbc54
thijsvl12 <thijsvl12@gmail.com>
2022-10-03 22:39:35 +0200

    Add cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './modules/app/app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.enableCors();
   app.setGlobalPrefix(process.env.API_PREFIX || '');
 
